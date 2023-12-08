c102a4daf3df7ec48af48a5e25b397d4d6c933a2
Tony Eder <tonyeder11@gmail.com>
2023-12-08 10:51:01 -0800

    first pass at Filter deliverable completed

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,34 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, setItems }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("")
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event){
+    setSearch(event)
+  }
 
-    return item.category === selectedCategory;
+  // filter items based on two separate criteria: selectedCategory and searh
+  // I have combined the the two conditions into a single return statement within the filter method that was already provided
+  // the new variables are true in either case 
+  // search using includes() method better UX
+  const itemsToDisplay = items.filter((item) => {
+    const categoryMatch = selectedCategory === "All" || item.category === selectedCategory;
+    const searchMatch = search === "" || item.name.includes(search);
+  
+    return categoryMatch && searchMatch;
   });
+  
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} search={search} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
